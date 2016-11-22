cb6e92eab2ce8cec97428a2ca92f51a5b73dc541
Manu Farfaro <manufarfaro@gmail.com>
2016-11-22 03:19:05 -0300

    added heroku database enviroment variables.

--- app/Config/database.php
+++ app/Config/database.php
@@ -1,15 +1,33 @@
 <?php
 class DATABASE_CONFIG {
 
-	public $default = array(
-		'datasource' => 'Database/Mysql',
-		'persistent' => false,
-		'host' => 'localhost',
-		'port' => 3306,
-		'login' => 'root',
-		'password' => '',
-		//'login' => 'sonoricstudios.c',
-		//'password' => 'Qkry9Rry',
-		'database' => 'sonoricstudios',
-	);
+    public $url;
+
+    public $default = array();
+
+    public function __construct() {
+        if(getenv("CLEARDB_DATABASE_URL")) {
+            $this->url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+            $this->default = array(
+                'datasource' => 'Database/Mysql',
+                'persistent' => false,
+                'host' => $this->url['host'],
+                'port' => 3306,
+                'login' => $this->url['user'],
+                'password' => $this->url['pass'],
+                'database' => substr($this->url['path'], 1)
+            );
+        } else {
+            $this->default = array(
+                'datasource' => 'Database/Mysql',
+                'persistent' => false,
+                'host' => 'localhost',
+                'port' => 3306,
+                'login' => 'root',
+                'password' => '',
+                'database' => 'sonoricstudios',
+            );
+        }
+    }
+
 }
